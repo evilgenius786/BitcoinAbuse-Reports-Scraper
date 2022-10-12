344231920526c48030101dba44643375c8974e18
Muhammad Hassan <786hassan777@gmail.com>
2022-10-13 00:30:52 +0500

    Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import os
 import time
+from threading import Semaphore, Thread
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,60 +10,70 @@ from translate import Translator
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
+thread_count = 10
+semaphore = Semaphore(thread_count)
+
 
 def getData(addr):
     file = f'./reports/{addr}.json'
     if os.path.exists(file):
         print(f'File {file} already exists')
         return
-    url = f"https://www.bitcoinabuse.com/reports/{addr}"
-    print(url)
-    driver.get(url)
-    try:
-        driver.maximize_window()
-    except:
-        pass
-    time.sleep(1)
-    driver.save_full_page_screenshot(f'./screenshots/{addr}.png')
-    soup = BeautifulSoup(driver.page_source, 'lxml')
-    data = {}
-    page_count = soup.find_all('a', {'class': 'page-link'})
-    if page_count:
-        print("Page count: ", page_count[-2].text)
-    for tr in soup.find('table', {"id": 'summary-table'}).find_all('tr'):
-        data[tr.find('th').text] = tr.find('td').text
-    data['Address'] = addr
-    reports = soup.find('table', {'class': 'table table-striped table-bordered table-responsive-lg'})
-    ths = reports.find().find_all('th')
-    data['reports'] = []
-    next_page = True
-    while next_page:
-        for tr in reports.find_all('tr'):
-            tds = tr.find_all('td')
-            report = {ths[i].text: tds[i].text for i in range(len(tds))}
-            if report != {}:
-                data['reports'].append(report)
-        next_page = soup.find('a', {'rel': 'next'})
-        if next_page:
-            soup = getSoup(next_page['href'])
-            print(next_page['href'])
-            reports = soup.find('table', {'class': 'table table-striped table-bordered table-responsive-lg'})
-    print(json.dumps(data, indent=4))
-    with open(file, 'w') as f:
-        json.dump(data, f, indent=4)
-    return data
+    with semaphore:
+        url = f"https://www.bitcoinabuse.com/reports/{addr}"
+        print(url)
+        driver.get(url)
+        try:
+            driver.maximize_window()
+        except:
+            pass
+        time.sleep(1)
+        driver.save_full_page_screenshot(f'./screenshots/{addr}.png')
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+        data = {}
+        page_count = soup.find_all('a', {'class': 'page-link'})
+        if page_count:
+            print("Page count: ", page_count[-2].text)
+        for tr in soup.find('table', {"id": 'summary-table'}).find_all('tr'):
+            data[tr.find('th').text] = tr.find('td').text
+        data['Address'] = addr
+        reports = soup.find('table', {'class': 'table table-striped table-bordered table-responsive-lg'})
+        ths = reports.find().find_all('th')
+        data['reports'] = []
+        next_page = True
+        while next_page:
+            for tr in reports.find_all('tr'):
+                tds = tr.find_all('td')
+                report = {ths[i].text: tds[i].text for i in range(len(tds))}
+                if report != {}:
+                    data['reports'].append(report)
+            next_page = soup.find('a', {'rel': 'next'})
+            if next_page:
+                soup = getSoup(next_page['href'])
+                print(next_page['href'])
+                reports = soup.find('table', {'class': 'table table-striped table-bordered table-responsive-lg'})
+        print(json.dumps(data, indent=4))
+        with open(file, 'w') as f:
+            json.dump(data, f, indent=4)
+        return data
 
 
 def processPages():
     next_page = 'https://www.bitcoinabuse.com/reports'
+    threads = []
     while next_page:
         print(next_page)
         soup = getSoup(next_page)
         for addr in soup.find_all('div', {'class': 'col-xl-4 col-md-6 mb-3'}):
-            getData(addr.find('a').text)
+            threads.append(Thread(target=getData, args=(addr.find('a').text,)))
+            threads[-1].start()
         next_page = soup.find('a', {'rel': 'next'})
         if next_page:
             next_page = next_page['href']
+    print("All pages done!! Now Waiting for threads to finish!!")
+    for t in threads:
+        t.join()
+    print("All done!!")
 
 
 def main():
